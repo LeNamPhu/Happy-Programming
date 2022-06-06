e05a27aff17af69047f1eb14518e3ac5e8b7e886
LeNamPhu <105411107+LeNamPhu@users.noreply.github.com>
2022-06-06 15:07:27 +0700

    Add files via upload

---
+++ HappyProgramming/src/java/DTO/Skill.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+/**
+ *
+ * @author admin
+ */
+public class Skill {
+
+    private String id;
+    private String name;
+    private int status;
+
+    public Skill(String id, String name, int status) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+}
