965c03481993a45f99a1ea6695f93198bb351614
LeNamPhu <105411107+LeNamPhu@users.noreply.github.com>
2022-06-25 14:21:02 +0700

    Add files via upload

---
+++ 250622.sql
@@ -0,0 +1,9 @@
+alter table Skill
+add image nvarchar(250);
+go
+alter table Mentor 
+add Job nvarchar(250)
+alter table Mentor 
+add Introducton nvarchar(250)
+alter table Mentor 
+add Service nvarchar(250)
