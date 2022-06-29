5647bb43471cd9a0786fbee20103bab092a12109
Chrije <105277658+Chrije@users.noreply.github.com>
2022-06-30 01:14:28 +0700

    Add files via upload

---
+++ CreateRequestController.java
@@ -0,0 +1,109 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import DAO.RequestDAO;
+import DTO.Account;
+import DTO.Request;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author trida
+ */
+@WebServlet(name = "CreateRequestController", urlPatterns = {"/CreateRequestController"})
+public class CreateRequestController extends HttpServlet {
+    private final String ERROR = "Error.jsp";
+    private final String SUCCESS = "CreateRequest.jsp";
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = ERROR;
+        try {
+            HttpSession session = request.getSession(true);
+            Account user = (Account) session.getAttribute("SIGNIN_ACCOUNT");
+             String title = request.getParameter("title");
+             String status = "Open";
+             String content = request.getParameter("content");
+             int menteeID = user.getId();
+             Date deadlineDate = Date.valueOf(request.getParameter("deadlineDate"));
+             int deadlineHour = Integer.parseInt(request.getParameter("deadlineHour"));
+            
+             boolean valid = true;
+             Request req = new Request(0, title, status, content, menteeID, deadlineDate, deadlineHour);
+             RequestDAO reqdao = new RequestDAO();
+             boolean checkinsert = reqdao.insertREQ(req);
+             if(checkinsert){
+                 url = SUCCESS;
+             }
+             
+             
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        finally{
+            request.getRequestDispatcher(url).forward(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ RequestDAO.java
@@ -0,0 +1,461 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import DTO.Request;
+import Utils.DBUtils;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author ThienNho
+ */
+public class RequestDAO {
+
+    public static final String LIST_REQUEST_BY_MENTEE = "SELECT ID, Title, Status, Content, DeadlineDate, DeadlineHour FROM Request WHERE MenteeID=?";
+    public static final String GET_SKILL_ID = "SELECT skillID FROM RequestSkill WHERE RequestID=?";
+    public static final String GET_SKILL_NAME = "SELECT Name FROM Skill WHERE ID=?";
+    public static final String DELETE_REQUEST = "UPDATE Request SET Status=? WHERE ID=?";
+    public static final String UPDATE_REQUEST = "UPDATE Request SET Title=?, Content=?, DeadlineDate=?, DeadlineHour=? WHERE ID=?";
+    public static final String DELETE_SKILL_REQUEST = "DELETE RequestSkill WHERE RequestID =? ";
+    public static final String INSERT_REQUEST = "INSERT INTO Request(Title, Status, Content, MenteeID, DeadlineDate, DeadlineHour) VALUES (?,?,?,?,?,?)";
+
+    public ArrayList<Request> listRequestByMentee(int menteeID) throws SQLException {
+        ArrayList<Request> list = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.makeConnection();
+            if (conn != null) {
+                String sql = LIST_REQUEST_BY_MENTEE;
+                stm = conn.prepareStatement(sql);
+                stm.setInt(1, menteeID);
+                rs = stm.executeQuery();
+                while (rs.next()) {
+                    if ("Open".equals(rs.getString("Status"))  || "Processing".equals(rs.getString("Status")) /*|| "Open".equals(rs.getString("Status"))*/) {
+                        int ID = rs.getInt("ID");
+                        String title = rs.getString("Title");
+                        String status = rs.getString("Status");
+                        String content = rs.getString("Content");
+                        Date deadlineDate = rs.getDate("DeadlineDate");
+                        int deadlineHour = rs.getInt("DeadlineHour");
+                        list.add(new Request(ID, title, status, content, menteeID, deadlineDate, deadlineHour));
+                    }
+                }
+
+            }
+        } catch (Exception e) {
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return list;
+    }
+
+    public ArrayList<Integer> getIDSkillReq(Request req) throws SQLException {
+        ArrayList<Integer> listID = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.makeConnection();
+            if (conn != null) {
+                String sql = GET_SKILL_ID;
+                stm = conn.prepareStatement(sql);
+                stm.setInt(1, req.getId());
+                rs = stm.executeQuery();
+                while (rs.next()) {
+                    int id = rs.getInt("skillID");
+                    listID.add(id);
+                }
+            }
+        } catch (Exception e) {
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return listID;
+    }
+
+    public void getNameSkillReq(ArrayList<Integer> listID, Request req, Map<Request, String> map) throws SQLException {
+        String listNameSkill = "";
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.makeConnection();
+            if (conn != null) {
+                for (Integer id : listID) {
+                    String sql = GET_SKILL_NAME;
+                    stm = conn.prepareStatement(sql);
+                    stm.setInt(1, id);
+                    rs = stm.executeQuery();
+                    if (rs.next()) {
+                        String skillName = rs.getString("Name");
+                        listNameSkill += skillName + " ";
+                    }
+                }
+                map.put(req, listNameSkill);
+            }
+        } catch (Exception e) {
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
+
+    public void deleteReq(int reqID) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.makeConnection();
+            if (conn != null) {
+                String sql = DELETE_REQUEST;
+                stm = conn.prepareStatement(sql);
+                stm.setString(1, "Cancel");
+                stm.setInt(2, reqID);
+                int value = stm.executeUpdate();
+            }
+        } catch (Exception e) {
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
+
+    public void updateReq(Request req) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.makeConnection();
+            if (conn != null) {
+                String sql = UPDATE_REQUEST;
+                stm = conn.prepareStatement(sql);
+                stm.setString(1, req.getTitle());             
+                stm.setString(2, req.getContent());
+                stm.setDate(3, req.getDeadlineDate());
+                stm.setInt(4, req.getDeadlineHour());
+                stm.setInt(5, req.getId());
+                int value = stm.executeUpdate();
+            }
+        } catch (Exception e) {
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
+
+    public void updateSkillReq(int reqID, String skill1, String skill2, String skill3) throws SQLException {
+        deleteSkillReq(reqID);
+        ArrayList<Integer> listSkillID = getSkillIDByName(skill1, skill2, skill3);
+        insertSkillIDToRequestSkill(reqID, listSkillID);
+    }
+
+    public void deleteSkillReq(int reqID) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.makeConnection();
+            if (conn != null) {
+                String sql = "DELETE RequestSkill WHERE RequestID =? ";
+                stm = conn.prepareStatement(sql);
+                stm.setInt(1, reqID);
+                int value = stm.executeUpdate();
+            }
+        } catch (Exception e) {
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
+
+    public ArrayList<Integer> getSkillIDByName(String skill1, String skill2, String skill3) throws SQLException {
+        ArrayList<String> listName = new ArrayList<>();
+        listName.add(skill1);
+        listName.add(skill2);
+        listName.add(skill3);
+        ArrayList<Integer> listSkillID = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.makeConnection();
+            if (conn != null) {
+                for (String Name : listName) {
+                    String sql = "SELECT ID FROM Skill WHERE Name=? and Status='active'";
+                    stm = conn.prepareStatement(sql);
+                    stm.setString(1, Name);
+                    rs = stm.executeQuery();
+                    if (rs.next()) {
+                        listSkillID.add(rs.getInt("ID"));
+                    }
+                }
+            }
+        } catch (Exception e) {
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+
+        return listSkillID;
+    }
+
+    public void insertSkillIDToRequestSkill(int reqID, ArrayList<Integer> listSkillID) throws SQLException {
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.makeConnection();
+            if (conn != null) {
+                for (Integer ID : listSkillID) {
+                    String sql = "INSERT INTO RequestSkill (SkillID,RequestID) VALUES (?,?)";
+                    stm = conn.prepareStatement(sql);
+                    stm.setInt(1, ID);
+                    stm.setInt(2, reqID);
+                     int value = stm.executeUpdate();
+                }
+            }
+        } catch (Exception e) {
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
+
+    public ArrayList<Integer> getListInviteRequestID(int id) throws SQLException{
+        ArrayList<Integer> listInviteRequestID = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.makeConnection();
+            if (conn != null) {
+                String sql = "SELECT ReqID FROM Invite WHERE MentorID =? and Status =?";
+                stm = conn.prepareStatement(sql);
+                stm.setInt(1, id);
+                stm.setString(2, "Processing");
+                rs = stm.executeQuery();
+                while(rs.next()){
+                    int reqID = rs.getInt("ReqID");
+                    listInviteRequestID.add(reqID);
+                }
+                
+            }
+        } catch (Exception e) {
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        
+        return listInviteRequestID;
+    }
+    
+    public ArrayList<Request> getListInviteRequest(ArrayList<Integer> listInviteRequestID) throws SQLException{
+        ArrayList<Request> listInviteRequest = new ArrayList<>();
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.makeConnection();
+            if (conn != null) {
+                for (Integer reqID : listInviteRequestID) {
+                    String sql = "SELECT Title, Content, MenteeID, DeadlineDate, DeadlineHour FROM Request WHERE ID=? and Status=?";
+                    stm = conn.prepareStatement(sql);
+                    stm.setInt(1, reqID);
+                    stm.setString(2, "Processing");
+                    rs = stm.executeQuery();
+                    if (rs.next()) {                        
+                        listInviteRequest.add(new Request(reqID, rs.getString("Title"), "Processiong", rs.getString("Content"), rs.getInt("MenteeID") , rs.getDate("DeadlineDate"), rs.getInt("DeadlineHour")));
+                    }
+                }
+                
+            }
+        } catch (Exception e) {
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        
+        return listInviteRequest;
+    }
+    
+    public void updateStatusRequest(int reqID, String status) throws SQLException{
+        Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.makeConnection();
+            if (conn != null) {
+                String sql = "UPDATE Request SET Status=? WHERE ID=? ";
+                stm = conn.prepareStatement(sql);
+                stm.setString(1, status);  
+                stm.setInt(2, reqID);                                         
+                int value = stm.executeUpdate();
+            }
+        } catch (Exception e) {
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+    }
+    
+    public ArrayList<Request> getListClosedReq(int menteeID) throws SQLException{
+         ArrayList<Request> list = new ArrayList<>();
+         Connection conn = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        try {
+            conn = DBUtils.makeConnection();
+            if (conn != null) {
+                String sql = LIST_REQUEST_BY_MENTEE;
+                stm = conn.prepareStatement(sql);
+                 stm.setInt(1, menteeID);
+                rs = stm.executeQuery();
+                while (rs.next()) {
+                    if ("Closed".equals(rs.getString("Status"))) {
+                        int ID = rs.getInt("ID");
+                        String title = rs.getString("Title");
+                        String status = rs.getString("Status");
+                        String content = rs.getString("Content");
+                        Date deadlineDate = rs.getDate("DeadlineDate");
+                        int deadlineHour = rs.getInt("DeadlineHour");
+                        list.add(new Request(ID, title, status, content, menteeID, deadlineDate, deadlineHour));
+                    }
+                }
+
+            }
+        } catch (Exception e) {
+        } finally {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stm != null) {
+                stm.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return list;
+    }
+    
+    public boolean insertREQ(Request req) throws SQLException{
+            boolean check = false;
+            Connection con = null;
+            PreparedStatement stm = null;
+            try{
+                con = DBUtils.makeConnection();
+                if(con!=null){
+                    stm = con.prepareStatement(INSERT_REQUEST);
+                    stm.setString(1, req.getTitle());
+                    stm.setString(2, req.getStatus());
+                    stm.setString(3, req.getContent());
+                    stm.setInt(4, req.getMenteeID());
+                    stm.setDate(5, req.getDeadlineDate());
+                    stm.setInt(6, req.getDeadlineHour());
+                    check = stm.executeUpdate()>0?true:false;
+                }
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+            finally{
+                if(stm!=null) stm.close();
+                if(con!=null) con.close();
+            }
+
+            return check;
+        }
+
+}
